8c5c0e9ba44d12ace52de603b7ec442ab77755ba
Bernhard Wagner <bernhard.wagner@sbs.ch>
2011-01-13 19:12:15 +0100

    VFormUtilTest: moved RegexTests to own compilation unit. VFormUtil: now using WordHierarchyBuilder.

--- src/ch/sbs/utils/preptools/vform/VFormUtil.java
+++ src/ch/sbs/utils/preptools/vform/VFormUtil.java
@@ -9,23 +9,112 @@ import javax.swing.text.Position;
 
 public class VFormUtil {
 
-	private static final String[] forms = new String[] { "Sie", "Ihrethalber",
-			"Ihretwegen", "Ihren", "Ihrem", "Ihres", "Ihre", "Ihr", "Ihnen", };
+	/*
+	 * Mail von Mischa 12.1.2011 
+	 * 3. Person PL
+	(obligatorische Abfrage)
 
-	private static final String[] formsOldSpelling = new String[] { "Du",
-			"Dich", "Dir", "Deinethalber", "Deinetwegen", "Deinen", "Deinem",
-			"Deines", "Deine", "Dein", "Eurethalber", "Euretwegen", "Euren",
-			"Eurem", "Eures", "Eure", "Euer", "Euch" };
+	Ihnen
+	Ihr
+	Ihre
+	Ihrem
+	Ihren
+	Ihrer
+	Ihrerseits
+	Ihres
+	Ihresgleichen
+	Ihrethalben
+	Ihretwegen
+	Ihretwillen
+	Ihrige
+	Ihrigem
+	Ihrigen
+	Ihriger
+	Ihriges
+	Ihrs
+	Sie
+	2. Person
+	(optionale Abfrage)
+
+	Dein
+	Deine
+	Deinem
+	Deinen
+	Deiner
+	Deinerseits
+	Deines
+	Deinesgleichen
+	Deinethalben
+	Deinetwegen
+	Deinetwillen
+	Deinige
+	Deinigem
+	Deinigen
+	Deiniger
+	Deiniges
+	Deins
+	Dich
+	Dir
+	Du
+	Euch
+	Euer
+	Euere
+	Euerem
+	Euerer
+	Eueres
+	Euers
+	Euerseits
+	Eure
+	Eurem
+	Euren
+	Eurerseits
+	Eures
+	Euresgleichen
+	Eurethalben
+	Euretwegen
+	Euretwillen
+	Eurige
+	Eurigem
+	Eurigen
+	Euriger
+	Euriges
+	 */
+
+	// Mail von Mischa Kuenzle 12.1.2011 15:09
+	// 3. Person PL (obligatorische Abfrage)
+	private static final String[] thirdPP = new String[] { "Ihnen", "Ihr", "Ihre",
+			"Ihrem", "Ihren", "Ihrer", "Ihrerseits", "Ihres", "Ihresgleichen",
+			"Ihrethalben", "Ihretwegen", "Ihretwillen", "Ihrige", "Ihrigem",
+			"Ihrigen", "Ihriger", "Ihriges", "Ihrs", "Sie", };
+
+	// Mail von Mischa Kuenzle 12.1.2011 15:09
+	// 2. Person (optionale Abfrage)
+	private static final String[] secondP = new String[] { "Dein", "Deine",
+			"Deinem", "Deinen", "Deiner", "Deinerseits", "Deines",
+			"Deinesgleichen", "Deinethalben", "Deinetwegen", "Deinetwillen",
+			"Deinige", "Deinigem", "Deinigen", "Deiniger", "Deiniges", "Deins",
+			"Dich", "Dir", "Du", "Euch", "Euer", "Euere", "Euerem", "Euerer",
+			"Eueres", "Euers", "Euerseits", "Eure", "Eurem", "Euren",
+			"Eurerseits", "Eures", "Euresgleichen", "Eurethalben",
+			"Euretwegen", "Euretwillen", "Eurige", "Eurigem", "Eurigen",
+			"Euriger", "Euriges", };
+
+	private static final String[] forms3rdPersonPlural = new String[] { WordHierarchyBuilder
+			.createWordTree(thirdPP).toRegex() };
+
+	private static final String[] forms2ndPerson = new String[] { WordHierarchyBuilder
+			.createWordTree(secondP).toRegex() };
 
 	private static final String[] allForms;
 
 	static {
-		allForms = new String[forms.length + formsOldSpelling.length];
+		allForms = new String[forms3rdPersonPlural.length
+				+ forms2ndPerson.length];
 		int i = 0;
-		for (final String form : forms) {
+		for (final String form : forms3rdPersonPlural) {
 			allForms[i++] = form;
 		}
-		for (final String form : formsOldSpelling) {
+		for (final String form : forms2ndPerson) {
 			allForms[i++] = form;
 		}
 	}
@@ -166,9 +255,9 @@ public class VFormUtil {
 	 *   Frage: Wenn Benutzer Dokument wechselt, müsste Toolbar angepasst werden.
 	 *   Problem: es gibt showToolbar (_3) aber nicht hideToolbar (_2)
 	 *   
-	 *  TODO: keys für Toolbar! (_4)
-	 *  TODO: Bereits markierte Form überspringen.
-	 *  TODO: Find VForms: muss sich merken, ob der Prozess neu gestartet wurde
+	 *  DONE: keys für Toolbar! (_4)
+	 *  DONE: Bereits markierte Form überspringen.
+	 *  DONE: Find VForms: muss sich merken, ob der Prozess neu gestartet wurde
 	 *        wenn Ja: Cursor an Dokumentanfang setzen (_5) und von dort suchen.
 	 *        
 	 *  Optional:
@@ -310,14 +399,29 @@ public class VFormUtil {
 	 * 
 	 */
 
-	private static final Pattern vFormPattern;
+	private static Pattern vFormDefaultPattern;
+	private static final Pattern vFormPatternAll;
+	private static final Pattern vFormPattern3rdPersonPlural;
 
 	static {
+		vFormPatternAll = makePattern(allForms);
+		vFormPattern3rdPersonPlural = makePattern(forms3rdPersonPlural);
+		vFormDefaultPattern = vFormPatternAll;
+	}
 
+	public static Pattern get3rdPPPattern() {
+		return vFormPattern3rdPersonPlural;
+	}
+
+	public static Pattern getAllPattern() {
+		return vFormPatternAll;
+	}
+
+	protected static Pattern makePattern(final String[] forms) {
 		final StringBuffer sb = new StringBuffer("(?:"); // non-capturing group,
 															// see
 		// http://download.oracle.com/javase/1.5.0/docs/api/java/util/regex/Pattern.html#special
-		for (final String form : allForms) {
+		for (final String form : forms) {
 			sb.append("(?<!<brl:v-form>)"); // negative lookbehind
 			sb.append(form);
 			sb.append("(?!</brl:v-form>)"); // negative lookahead
@@ -325,23 +429,80 @@ public class VFormUtil {
 		}
 		sb.setLength(sb.length() - 1); // chop off last "|"
 		sb.append(")\\b"); // make sure we don't match substrings.
-		vFormPattern = Pattern.compile(sb.toString());
+		return Pattern.compile(sb.toString());
 	}
 
-	public static String replace(final String theText) {
-		return vFormPattern.matcher(theText).replaceAll(
+	/**
+	 * Returns the given text with matching parts surrounded by
+	 * <brl:v-form></brl:v-form>
+	 * 
+	 * @param theText
+	 *            the text to work on
+	 * @param pattern
+	 *            the pattern to match
+	 * @return the given text with matching parts surrounded by
+	 *         <brl:v-form></brl:v-form>
+	 */
+	public static String replace(final String theText, final Pattern pattern) {
+		return pattern.matcher(theText).replaceAll(
 				"<brl:v-form>$0</brl:v-form>");
 		// Group zero always stands for the entire expression.
 		// http://download.oracle.com/javase/1.5.0/docs/api/index.html?java/util/regex/Matcher.html
 	}
 
-	public static Match find(final String text, int i) {
-		final Matcher m = vFormPattern.matcher(text);
+	/**
+	 * Does the same as @see replace using the default pattern.
+	 * 
+	 * @param theText
+	 * @return
+	 */
+	public static String replace(final String theText) {
+		return replace(theText, vFormDefaultPattern);
+	}
+
+	/**
+	 * Returns Match where the pattern occurs or NULL_MATCH
+	 * 
+	 * @param text
+	 * @param i
+	 * @param pattern
+	 * @return
+	 */
+	public static Match find(final String text, int i, final Pattern pattern) {
+		final Matcher m = pattern.matcher(text);
 		return m.find(i) ? new Match(m.start(), m.end()) : NULL_MATCH;
 	}
 
+	/**
+	 * Does the same as @see find using the default pattern.
+	 * 
+	 * @param text
+	 * @param i
+	 * @return
+	 */
+	public static Match find(final String text, int i) {
+		return find(text, i, vFormDefaultPattern);
+	}
+
+	/**
+	 * Indicates whether text matches pattern
+	 * 
+	 * @param text
+	 * @param pattern
+	 * @return
+	 */
+	public static boolean matches(final String text, final Pattern pattern) {
+		return text != null && pattern.matcher(text).matches();
+	}
+
+	/**
+	 * Does the same as matches for the default pattern
+	 * 
+	 * @param text
+	 * @return
+	 */
 	public static boolean matches(final String text) {
-		return text != null && vFormPattern.matcher(text).matches();
+		return matches(text, vFormDefaultPattern);
 	}
 
 	public static class PositionMatch {
--- test_src/ch/sbs/plugin/preptools/VFormUtilTest.java
+++ test_src/ch/sbs/plugin/preptools/VFormUtilTest.java
@@ -5,9 +5,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-
 import org.junit.Test;
 
 import ch.sbs.utils.preptools.vform.VFormUtil;
@@ -174,197 +171,30 @@ public class VFormUtilTest {
 	10. Punkt QuoteSign ClosingBrace Whitespace QuoteSign 
 	 */
 
-	/*
-	 * 
-	 *             +----------+----------+
-	 *             |     =    |     !    |
-	 *             | Positive | Negative |
-	 * +-----------+----------+----------+
-	 * | Ahead  ?  |    ?=    |    ?!    |
-	 * +-----------+----------+----------+
-	 * | Behind ?< |   ?<=    |   ?<!    |
-	 * +-----------+----------+----------+
-	 */
-
-	// see java.util.regex.Pattern:2488: lookbehind must have fixed length
-	@Test(expected = PatternSyntaxException.class)
-	public void testPositiveLookBehindMustHaveFixedLength() {
-		Pattern.compile("(?<=k\\s*)");
-	}
-
-	// see java.util.regex.Pattern:2488: lookbehind must have fixed length
-	@Test(expected = PatternSyntaxException.class)
-	public void testNegativeLookBehindMustHaveFixedLength() {
-		Pattern.compile("(?<!k\\s*)");
-	}
-
 	@Test
-	public void testRegexNegation() {
+	public void testSettingPatternAll() {
+		// ____________________________1_________2_________3
+		// __________________01234567890123456789012345678901234
+		final String text = "Dann kann Anna es Deinem Kollegen geben.";
 
-		final String regex = "(?<!bar)foo(?!bar)";
-		final Pattern pattern = Pattern.compile(regex);
+		VFormUtil.Match match = VFormUtil.find(text, 0,
+				VFormUtil.getAllPattern());
 
-		assertFalse(pattern.matcher("barfoobar").find());
-		assertFalse(pattern.matcher("barfoo").find());
-		assertFalse(pattern.matcher("foobar").find());
-
-		assertTrue(pattern.matcher("foo").find());
-		assertTrue(pattern.matcher("bar foo bar").find());
-	}
-
-	@Test
-	public void testRegexNegationvForm() {
-
-		// final String regex =
-		// "(?<!<\\s*brl\\s*:\\s*vform\\s*>\\s*)Sie(?!<\\s*/\\s*brl\\s*:\\s*vform\\s*>)";
-		final String regex = "(?<!<brl:v-form>)Sie(?!</brl:v-form>)";
-		final Pattern pattern = Pattern.compile(regex);
-		assertFalse(pattern.matcher(
-				"Das können <brl:v-form>Sie</brl:v-form> nicht.").find());
-		assertFalse(pattern.matcher(
-				"Das können <brl:v-form> Sie</brl:v-form> nicht.").find());
-		assertFalse(pattern.matcher(
-				"Das können <brl:v-form>Sie </brl:v-form> nicht.").find());
-
-		assertTrue(pattern.matcher(
-				"Das können <brl:v-form> Sie </brl:v-form> nicht.").find());
-
-		assertTrue(pattern.matcher(
-				"Das können <brl:v-form >Sie </brl:v-form> nicht.").find());
+		assertEquals(18, match.startOffset);
+		assertEquals(24, match.endOffset);
 
 	}
 
 	@Test
-	public void testRegexNegationvFormWithSpaces() {
+	public void testSettingPattern3rdPP() {
+		// ____________________________1_________2_________3
+		// __________________01234567890123456789012345678901234
+		final String text = "Dann kann Anna es Deinem Kollegen geben.";
 
-		// final String regex =
-		// "(?<!<\\s*brl\\s*:\\s*vform\\s*>\\s*)Sie(?!<\\s*/\\s*brl\\s*:\\s*vform\\s*>)";
-		final String regex = "(?<!<brl:v-form>)Sie(?!<\\s*/\\s*brl\\s*:\\s*v-form\\s*>)";
-		final Pattern pattern = Pattern.compile(regex);
-		assertFalse(pattern.matcher(
-				"Das können <   brl:v-form>Sie</brl:v-form> nicht.").find());
-		assertFalse(pattern.matcher(
-				"Das können <brl:v-form> Sie</brl:v-form> nicht.").find());
-		assertFalse(pattern.matcher(
-				"Das können <brl:v-form>Sie </brl:v-form> nicht.").find());
+		VFormUtil.Match match = VFormUtil.find(text, 0,
+				VFormUtil.get3rdPPPattern());
 
-		assertTrue(pattern.matcher(
-				"Das können <brl:v-form> Sie </brl:v-form> nicht.").find());
-
-		assertTrue(pattern.matcher(
-				"Das können <brl:v-form >Sie </brl:v-form> nicht.").find());
-
-	}
-
-	@Test
-	public void testNegativeLookahead() { // (?! ___ )
-		// This regex asks:
-		// Is there "John, Blank, a word that's not Smith, a capital letter
-		// and a collection of word chars?
-		final String regex = "John (?!Smith)[A-Z]\\w+";
-		final Pattern pattern = Pattern.compile(regex);
-
-		assertFalse(pattern.matcher("John Smith").find());
-		assertTrue(pattern.matcher("John Jackson").find());
-		assertTrue(pattern.matcher("John Westling").find());
-		assertTrue(pattern.matcher("John Holmes").find());
-
-	}
-
-	@Test
-	public void testPositiveLookAhead() { // (?= ___ )
-		// This regex asks:
-		// Is there a string that starts with 255 ?
-		final String regex = "(?=255).*";
-		final Pattern pattern = Pattern.compile(regex);
-
-		assertTrue(pattern.matcher("255.0.0.1").find());
-		assertTrue(pattern.matcher(" 255.0.0.1").find());
-		assertTrue(pattern.matcher("255").find());
-		assertTrue(pattern.matcher(" 255").find());
-		assertFalse(pattern.matcher("25").find());
-		assertEquals(
-				"http://<ho>255.0.0.1. There, </ho>",
-				pattern.matcher("http://255.0.0.1. There, ").replaceAll(
-						"<ho>$0</ho>"));
-		assertEquals("<ho>255.0.0.1. There, </ho>",
-				pattern.matcher("255.0.0.1. There, ").replaceAll("<ho>$0</ho>"));
-	}
-
-	@Test
-	public void testPositiveLookAheadWithAnchor() { // (?= ___ )
-		// This regex asks:
-		// Is there a string that starts with 255 ?
-		final String regex = "(?=^255).*";
-		final Pattern pattern = Pattern.compile(regex);
-
-		assertTrue(pattern.matcher("255.0.0.1").find());
-		assertFalse(pattern.matcher(" 255.0.0.1").find());
-		assertTrue(pattern.matcher("255").find());
-		assertFalse(pattern.matcher(" 255").find());
-		assertFalse(pattern.matcher("25").find());
-		assertEquals(
-				"http://255.0.0.1. There, ",
-				pattern.matcher("http://255.0.0.1. There, ").replaceAll(
-						"<ho>$0</ho>"));
-		assertEquals("<ho>255.0.0.1. There, </ho>",
-				pattern.matcher("255.0.0.1. There, ").replaceAll("<ho>$0</ho>"));
-	}
-
-	@Test
-	public void testPositiveLookBehind() { // (?<= ___ )
-		// This regex asks:
-		// Is there a non white space sequence preceded by http:// ?
-		final String regex = "(?<=http://)\\S+";
-		final Pattern pattern = Pattern.compile(regex);
-
-		assertFalse(pattern.matcher("The Java2s website is found at ").find());
-		assertTrue(pattern.matcher("http://www.java2s.com. There, ").find());
-		assertFalse(pattern.matcher("you can find some Java examples.").find());
-		assertEquals(
-				"http://<ho>www.java2s.com.</ho> There, ",
-				pattern.matcher("http://www.java2s.com. There, ").replaceAll(
-						"<ho>$0</ho>"));
-	}
-
-	@Test
-	public void testNegativeLookBehind() { // (?<! ___ )
-		// http://www.lexemetech.com/2007/10/zero-width-negative-lookbehind.html
-		// This regex asks:
-		// Is there a number not preceded by + or - ?
-		final String regex = "(?<![-+\\d])(\\d+)";
-		final Pattern pattern = Pattern.compile(regex);
-
-		assertFalse(pattern.matcher("+152 -444").find());
-		assertTrue(pattern.matcher("+152 423 -444 -3 3").find());
-		assertEquals("+152 <ho>423</ho> -444", pattern.matcher("+152 423 -444")
-				.replaceAll("<ho>$1</ho>"));
-		assertEquals("+152 +423 -444", pattern.matcher("+152 423 -444")
-				.replaceAll("+$1"));
-	}
-
-	@Test
-	public void testPhraseEndPW() {
-
-	}
-
-	@Test
-	public void testPhraseEndPWQ() {
-
-	}
-
-	@Test
-	public void testPhraseEndPQW() {
-
-	}
-
-	@Test
-	public void testPhraseEndPQWQ() {
-
-	}
-
-	@Test
-	public void testPhraseEndPCW() {
+		assertEquals(match, VFormUtil.NULL_MATCH);
 
 	}
 }
