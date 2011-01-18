f05d0cfb84bc46094cf16e190ffbbb8c5a1bfbd7
Bernhard Wagner <bernhard.wagner@sbs.ch>
2011-01-18 16:57:30 +0100

    fixed package structure

---
+++ src/ch/sbs/utils/preptools/Match.java
@@ -0,0 +1,36 @@
+package ch.sbs.utils.preptools;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Position;
+
+public class Match {
+
+	public static class PositionMatch {
+		public PositionMatch(final Document theDocument, final Match match) {
+			this(theDocument, match.startOffset, match.endOffset);
+		}
+
+		public PositionMatch(final Document theDocument, int start, int end) {
+			try {
+				document = theDocument;
+				startOffset = document.createPosition(start);
+				endOffset = document.createPosition(end);
+			} catch (BadLocationException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		public Document document;
+		public Position startOffset;
+		public Position endOffset;
+	}
+
+	public Match(int start, int end) {
+		startOffset = start;
+		endOffset = end;
+	}
+
+	public int startOffset;
+	public int endOffset;
+}
