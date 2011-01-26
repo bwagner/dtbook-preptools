f449d677c5e6aedd5ae5a50975b9b6f077aea110
Bernhard Wagner <bernhard.wagner@sbs.ch>
2011-01-26 18:56:22 +0100

    implemented findOrphans according to Mischa Kuenzle 2011-01-25

--- src/ch/sbs/utils/preptools/parens/ParensUtil.java
+++ src/ch/sbs/utils/preptools/parens/ParensUtil.java
@@ -2,10 +2,8 @@ package ch.sbs.utils.preptools.parens;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,29 +24,189 @@ public class ParensUtil {
 	public static final char LPAREN = '(';
 	public static final char RPAREN = ')';
 
-	public static final char[] ALL_CHARS = new char[] { LAQUO, RAQUO, LSAQUO,
-			RSAQUO, LANG, RANG, LBRACE, RBRACE, LBRACKET, RBRACKET, LPAREN,
-			RPAREN, };
+	static abstract class OrphanMatcher {
 
-	public static final String OPENING = LAQUO + "|" + LSAQUO + "|" + LANG
-			+ "|\\" + LBRACE + "|\\" + LBRACKET + "|\\" + LPAREN;
+		private static final Pattern skipPattern;
 
-	public static final String CLOSING = RAQUO + "|" + RSAQUO + "|" + RANG
-			+ "|\\" + RBRACE + "|\\" + RBRACKET + "|\\" + RPAREN;
+		static {
+			// http://download.oracle.com/javase/tutorial/essential/regex/quant.html
+			final String OPENING_TAG = "<\\s*brl\\s*:\\s*literal";
+			final String OPTIONAL_ARG = "(?:\\s+brl\\s*:grade\\s*=\\s*\"\\d\")?";
+			final String CLOSING_ANGLE = ">";
+			final String NON_GREEDY_CONTENT = ".*?";
+			final String CLOSING_TAG = "<\\s*/\\s*brl\\s*:\\s*literal\\s*>";
+			final StringBuilder sb = new StringBuilder();
+			sb.append(OPENING_TAG);
+			sb.append(OPTIONAL_ARG);
+			sb.append(CLOSING_ANGLE);
+			sb.append(NON_GREEDY_CONTENT);
+			sb.append(CLOSING_TAG);
+			skipPattern = Pattern.compile(sb.toString());
+		}
 
-	public static final String ALL = OPENING + "|" + CLOSING;
+		private final List<Match> regionsToSkip;
 
-	public static final Pattern pattern = Pattern.compile(ALL);
+		private void findRegionsToSkip(final String theText) {
+			final Matcher matcher = skipPattern.matcher(theText);
+			while (matcher.find()) {
+				regionsToSkip.add(new Match(matcher.start(), matcher.end()));
+			}
+		}
 
-	public static final Map<Character, Character> pairs = new HashMap<Character, Character>();
+		private boolean inSkipRegion(final Matcher matcher) {
+			boolean inSkipRegion = false;
+			final Iterator<Match> it = regionsToSkip.iterator();
+			while (!inSkipRegion && it.hasNext()) {
+				final Match skipRegion = it.next();
+				inSkipRegion = skipRegion.startOffset <= matcher.start()
+						&& skipRegion.endOffset > matcher.start();
+			}
+			return inSkipRegion;
+		}
 
-	static {
-		pairs.put(LAQUO, RAQUO);
-		pairs.put(LSAQUO, RSAQUO);
-		pairs.put(LANG, RANG);
-		pairs.put(LBRACE, RBRACE);
-		pairs.put(LBRACKET, RBRACKET);
-		pairs.put(LPAREN, RPAREN);
+		public OrphanMatcher() {
+			regionsToSkip = new ArrayList<Match>();
+		}
+
+		public List<Match> findOrphans(final String theText) {
+			findRegionsToSkip(theText);
+			final List<Match> orphans = new ArrayList<Match>();
+			final String[][] patternPairs = getPatternPairs();
+			for (final String[] patPair : patternPairs) {
+				final StringBuilder sb = new StringBuilder();
+				sb.append(escape(patPair[0]));
+				sb.append("|");
+				sb.append(escape(patPair[1]));
+				final Pattern pattern = Pattern.compile(sb.toString());
+				final Matcher matcher = pattern.matcher(theText);
+
+				// must be called before closingChar!
+				final char openingChar = getOpeningChar(matcher, patPair);
+
+				final char closingChar = getClosingChar(matcher, patPair);
+				boolean expectOpening = true;
+				Match match = null;
+				Match previousMatch = null;
+				matcher.reset();
+				while (matcher.find()) {
+					if (inSkipRegion(matcher)) {
+						continue;
+					}
+					final char matchChar = matcher.group().charAt(0);
+					previousMatch = match;
+					match = new Match(matcher.start(), matcher.end());
+					if (expectOpening) {
+						if (matchChar == closingChar) {
+							// error. Since it's closing brace we leave
+							// expectation to opening next. expectOpening ==
+							// true (no change)
+							orphans.add(match);
+						}
+						else {
+							expectOpening = false;
+						}
+					}
+					else {
+						if (matchChar == openingChar) {
+							// error. Since it's opening brace we leave
+							// expectation to closing next. expectOpening ==
+							// false (no change) We point to the previous match
+							// that is the brace with the missing closing brace.
+							orphans.add(previousMatch);
+						}
+						else {
+							expectOpening = true;
+						}
+					}
+				}
+				if (!expectOpening) {
+					orphans.add(match);
+				}
+			}
+			return orphans;
+		}
+
+		protected abstract char getOpeningChar(final Matcher matcher,
+				final String[] patPair);
+
+		protected abstract char getClosingChar(final Matcher matcher,
+				final String[] patPair);
+
+		protected abstract String[][] getPatternPairs();
+
+		protected String escape(final String str) {
+			return str;
+		}
+	}
+
+	static class QuoteOrphanMatcher extends OrphanMatcher {
+
+		private static final char UNSET_CHAR = 0;
+		private char openingChar = UNSET_CHAR;
+		private char closingChar = UNSET_CHAR;
+
+		@Override
+		protected String[][] getPatternPairs() {
+			return new String[][] { { "«", "»" }, { "‹", "›" }, { "〈", "〉" }, };
+		}
+
+		@Override
+		protected char getOpeningChar(final Matcher matcher,
+				final String[] patPair) {
+			if (openingChar != UNSET_CHAR) {
+				return openingChar;
+			}
+			if (!matcher.find()) {
+				return openingChar;
+			}
+			openingChar = matcher.group().charAt(0);
+			closingChar = patPair[patPair[0].charAt(0) == openingChar ? 1 : 0]
+					.charAt(0);
+			return openingChar;
+		}
+
+		@Override
+		protected char getClosingChar(final Matcher matcher,
+				final String[] patPair) {
+			return closingChar;
+		}
+	}
+
+	static class ParensOrphanMatcher extends OrphanMatcher {
+
+		@Override
+		protected String[][] getPatternPairs() {
+			return new String[][] { { "{", "}" }, { "[", "]" }, { "(", ")" }, };
+		}
+
+		@Override
+		protected char getOpeningChar(final Matcher matcher,
+				final String[] patPair) {
+			return patPair[0].charAt(0);
+		}
+
+		@Override
+		protected char getClosingChar(final Matcher matcher,
+				final String[] patPair) {
+			return patPair[1].charAt(0);
+		}
+
+		@Override
+		protected String escape(final String str) {
+			return "\\" + str;
+		}
+	}
+
+	static List<Match> findOrphansQuotes(final String theText) {
+		final List<Match> findOrphans = new QuoteOrphanMatcher()
+				.findOrphans(theText);
+		return findOrphans;
+	}
+
+	static List<Match> findOrphansParens(final String theText) {
+		final List<Match> findOrphans = new ParensOrphanMatcher()
+				.findOrphans(theText);
+		return findOrphans;
 	}
 
 	/**
@@ -59,49 +217,10 @@ public class ParensUtil {
 	 * @return list of potentially orphaned parens. It can be empty.
 	 */
 	public static List<Match> findOrphans(final String theText) {
-
-		// for each paren, keep a list of matches.
-		final Map<Character, List<Match>> occurrences = new HashMap<Character, List<Match>>();
-
-		for (final char ch : ALL_CHARS) {
-			occurrences.put(ch, new ArrayList<Match>());
-		}
-		final Matcher matcher = pattern.matcher(theText);
-
-		while (matcher.find()) {
-			final char matchChar = matcher.group().charAt(0);
-			final Match match = new Match(matcher.start(), matcher.end());
-			occurrences.get(matchChar).add(match);
-		}
-		final List<Match> potentialOrphans = new ArrayList<Match>();
-		for (final char ch : pairs.keySet()) {
-			if (occurrences.get(ch).size() == occurrences.get(pairs.get(ch))
-					.size()) {
-				occurrences.remove(ch);
-				occurrences.remove(pairs.get(ch));
-			}
-		}
-		for (final List<Match> occurence : occurrences.values()) {
-			potentialOrphans.addAll(occurence);
-		}
-
-		// TODO: it's kinda stupid to order them by appearance if we
-		// have this information while gathering the matches.
-		// The difficulty is that matches will get deleted in between.
-		Collections.sort(potentialOrphans, new Comparator<Match>() {
-			/**
-			 * Returns a negative integer, zero, or a positive integer as the
-			 * first argument is less than, equal to, or greater than the second
-			 * 
-			 * @param o1
-			 * @param o2
-			 * @return
-			 */
-			@Override
-			public int compare(final Match o1, final Match o2) {
-				return o1.startOffset - o2.startOffset;
-			}
-		});
-		return potentialOrphans;
+		final List<Match> orphans = new QuoteOrphanMatcher()
+				.findOrphans(theText);
+		orphans.addAll(new ParensOrphanMatcher().findOrphans(theText));
+		Collections.sort(orphans);
+		return orphans;
 	}
 }
--- test_src/ch/sbs/utils/preptools/parens/ParensUtilTest.java
+++ test_src/ch/sbs/utils/preptools/parens/ParensUtilTest.java
@@ -2,6 +2,7 @@ package ch.sbs.utils.preptools.parens;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -45,5 +46,313 @@ public class ParensUtilTest {
 		match = orphans.get(i++);
 		assertEquals(19, match.startOffset);
 		assertEquals(20, match.endOffset);
+	}
+
+	@Test
+	public void testParensLoc2() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "« » ‹ › 〈 (    [ ] {  ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(3, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(8, match.startOffset);
+		assertEquals(9, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(10, match.startOffset);
+		assertEquals(11, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(19, match.startOffset);
+		assertEquals(20, match.endOffset);
+	}
+
+	@Test
+	public void testParensLoc3() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "« » ‹ › 〈 ( )) (( [ ] { } ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(4, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(8, match.startOffset);
+		assertEquals(9, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(13, match.startOffset);
+		assertEquals(14, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(15, match.startOffset);
+		assertEquals(16, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(16, match.startOffset);
+		assertEquals(17, match.endOffset);
+	}
+
+	@Test
+	public void testParensLocMix() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "(( )) (( [   } ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(6, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(0, match.startOffset);
+		assertEquals(1, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(4, match.startOffset);
+		assertEquals(5, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(6, match.startOffset);
+		assertEquals(7, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(7, match.startOffset);
+		assertEquals(8, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(9, match.startOffset);
+		assertEquals(10, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(13, match.startOffset);
+		assertEquals(14, match.endOffset);
+	}
+
+	@Test
+	public void testParensLoc4() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "(( ))";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(2, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(0, match.startOffset);
+		assertEquals(1, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(4, match.startOffset);
+		assertEquals(5, match.endOffset);
+	}
+
+	@Test
+	public void testParensLocQuot() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "«« »»";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(2, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(0, match.startOffset);
+		assertEquals(1, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(4, match.startOffset);
+		assertEquals(5, match.endOffset);
+	}
+
+	@Test
+	public void testParensLocQuot2() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "«« »» ‹ › 〈 〉 ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(2, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(0, match.startOffset);
+		assertEquals(1, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(4, match.startOffset);
+		assertEquals(5, match.endOffset);
+	}
+
+	@Test
+	public void testParensLocQuot3() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "« » ‹ › 〈 〉 ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot4() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "〈 › « ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(3, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(0, match.startOffset);
+		assertEquals(1, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(2, match.startOffset);
+		assertEquals(3, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals(4, match.startOffset);
+		assertEquals(5, match.endOffset);
+	}
+
+	@Test
+	public void testParensLocQuot5() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "« › » ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(1, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals(2, match.startOffset);
+		assertEquals(3, match.endOffset);
+	}
+
+	@Test
+	public void testParensLocQuotBrlLiteral1() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<brl:literal>›</brl:literal>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuotBrlLiteral2() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<brl:literal>(</brl:literal>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuotBrlLiteral3() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<brl:literal brl:grade=\"0\">(</brl:literal>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuotBrlLiteral4() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<brl:literal brl:grade=\"1\">(</brl:literal>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuotBrlLiteral5() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<brl:literal brl:grade=\"2\">(</brl:literal>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuotBrlLiteral() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<brl:literal>« › »</brl:literal>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		Collections.sort(orphans);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot6() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "« ‹ 〈 〉 › » ";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot7() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "Du, (<brl:literal>(!Sie </brl:literal> wert <brl:literal brl:grade=\"2\">und)]}</brl:literal>) [Eure, Ihre";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(1, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot7a() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "(<brl:literal></brl:literal>)";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot8() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+				+ "<dtbook>"
+				+ "<section>"
+				+ "<oh style=\"\"     bla=\"z\"> 2"
+				+ "Du, (<brl:literal>(!Sie </brl:literal> wert <brl:literal brl:grade=\"2\">und)]}</brl:literal>) [Eure, Ihre"
+				+ "</oh><ol>" + "<li>a) «bla»</li>" + "<li>b) «blu»</li></ol>"
+				+ "{ bla }" + "</section>" + "</dtbook>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(3, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot8a() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "" + "( wert ) [Eure, Ihre" + "<li>a)</li>"
+				+ "<li>b)</li></ol>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(3, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot9() {
+		// ------------------------------1---------2
+		// --------------------012345678901234567890123456789
+		final String sample = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+				+ "<dtbook>" + " <brl:literal brl:grade=\"0\">)</brl:literal>"
+				+ "</dtbook>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(0, orphans.size());
+	}
+
+	@Test
+	public void testParensLocQuot10() {
+		final String head = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+				+ "<dtbook>";
+		final String middle = "(<brl:literal></brl:literal>)";
+		// ----------------------------------------------1---------2
+		// ------------------------------------012345678901234567890123456789
+		final String sample = head + middle + "<li>a)</li><li>b)</li>"
+				+ "</dtbook>";
+		final List<Match> orphans = ParensUtil.findOrphans(sample);
+		assertEquals(2, orphans.size());
+		int i = 0;
+		Match match = orphans.get(i++);
+		assertEquals((head + middle).length() + 5, match.startOffset);
+		assertEquals((head + middle).length() + 6, match.endOffset);
+		match = orphans.get(i++);
+		assertEquals((head + middle).length() + 16, match.startOffset);
+		assertEquals((head + middle).length() + 17, match.endOffset);
 	}
 }
