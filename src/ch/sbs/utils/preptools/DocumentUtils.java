ff6e35ad666cc39aebde06fa8688fcac8e37cade
Bernhard Wagner <bernhard.wagner@sbs.ch>
2011-07-07 14:32:15 +0200

    replaced new Matcher by matcher.reset

--- src/ch/sbs/utils/preptools/DocumentUtils.java
+++ src/ch/sbs/utils/preptools/DocumentUtils.java
@@ -12,27 +12,28 @@ public class DocumentUtils {
 	 * starting at document index theStart. The replacement string may contain
 	 * backreferences (e.g. "$1").
 	 * 
-	 * @param document
+	 * @param theDocument
 	 *            The document on which to perform the replacement.
-	 * @param regex
+	 * @param theRegexToSearch
 	 *            The regex for which to search.
-	 * @param replacement
+	 * @param theReplacement
 	 *            The replacement for the found regex, may include
 	 *            backreferences (e.g. "$1")
 	 * @return The number of replacements applied.
 	 */
-	public static int performReplacement(final Document document,
-			final String regex, final String replacement) {
-		return performReplacement(document, regex, replacement, 0);
+	public static int performReplacement(final Document theDocument,
+			final String theRegexToSearch, final String theReplacement) {
+		return performReplacement(theDocument, theRegexToSearch,
+				theReplacement, 0);
 	}
 
 	/**
 	 * Performs as many replacements as possible of pattern regex in document,
 	 * The replacement string may contain backreferences (e.g. "$1").
 	 * 
-	 * @param document
+	 * @param theDocument
 	 *            The document on which to perform the replacement.
-	 * @param regex
+	 * @param theRegexToSearch
 	 *            The regex for which to search.
 	 * @param replacement
 	 *            The replacement for the found regex, may include
@@ -41,24 +42,21 @@ public class DocumentUtils {
 	 *            The index from where on to start the search.
 	 * @return The number of replacements applied.
 	 */
-	public static int performReplacement(final Document document,
-			final String regex, final String replacement, int theStart) {
-		final Pattern pattern = Pattern.compile(regex);
+	public static int performReplacement(final Document theDocument,
+			final String theRegexToSearch, final String theReplacement,
+			int theStart) {
+		final Pattern pattern = Pattern.compile(theRegexToSearch);
 		int replacements = 0;
 		try {
-			Matcher matcher;
+			final Matcher matcher = pattern.matcher(theDocument.getText(0,
+					theDocument.getLength()));
 			int start = theStart;
-			while ((matcher = pattern.matcher(document.getText(0,
-					document.getLength()))).find(start)) {
+			while (matcher.find(start)) {
 				++replacements;
-				start = matcher.start();
-				final int length = matcher.end() - start;
-				final String matchedText = document.getText(start, length);
-				document.remove(start, length);
-				final Matcher matcher2 = pattern.matcher(matchedText);
-				final String replaceAll = matcher2.replaceAll(replacement);
-				document.insertString(start, replaceAll, null);
-				start += replaceAll.length();
+				start = doReplacements(theDocument, pattern, matcher,
+						theReplacement);
+				// need to reset matcher since document has changed.
+				matcher.reset(theDocument.getText(0, theDocument.getLength()));
 			}
 		} catch (final BadLocationException e) {
 			throw new RuntimeException(e);
@@ -71,39 +69,48 @@ public class DocumentUtils {
 	 * document index theStart. The replacement string may contain
 	 * backreferences (e.g. "$1").
 	 * 
-	 * @param document
+	 * @param theDocument
 	 *            The document on which to perform the replacement.
 	 * @param regex
 	 *            The regex for which to search.
-	 * @param replacement
+	 * @param theReplacement
 	 *            The replacement for the found regex, may include
 	 *            backreferences (e.g. "$1")
 	 * @param theStart
 	 *            The index from where on to start the search.
 	 * @return True if a replacement was applied, false otherwise.
 	 */
-	public static boolean performSingleReplacement(final Document document,
-			final String regex, final String replacement, int theStart) {
+	public static boolean performSingleReplacement(final Document theDocument,
+			final String regex, final String theReplacement, int theStart) {
 		final Pattern pattern = Pattern.compile(regex);
-		int replacements = 0;
+		boolean performedReplacement = false;
 		try {
-			Matcher matcher;
-			int start = theStart;
-			if ((matcher = pattern.matcher(document.getText(0,
-					document.getLength()))).find(start)) {
-				++replacements;
-				start = matcher.start();
-				final int length = matcher.end() - start;
-				final String matchedText = document.getText(start, length);
-				document.remove(start, length);
-				final Matcher matcher2 = pattern.matcher(matchedText);
-				final String replaceAll = matcher2.replaceAll(replacement);
-				document.insertString(start, replaceAll, null);
-				start += replaceAll.length();
+			final Matcher matcher = pattern.matcher(theDocument.getText(0,
+					theDocument.getLength()));
+			if (performedReplacement = matcher.find(theStart)) {
+				doReplacements(theDocument, pattern, matcher, theReplacement);
 			}
 		} catch (final BadLocationException e) {
 			throw new RuntimeException(e);
 		}
-		return replacements > 0;
+		return performedReplacement;
+	}
+
+	private static int doReplacements(final Document theDocument,
+			final Pattern thePattern, final Matcher theMatcher,
+			final String theReplacement) {
+		int start = theMatcher.start();
+		final int length = theMatcher.end() - start;
+		try {
+			final String matchedText = theDocument.getText(start, length);
+			theDocument.remove(start, length);
+			final String replaceAll = thePattern.matcher(matchedText)
+					.replaceAll(theReplacement);
+			theDocument.insertString(start, replaceAll, null);
+			start += replaceAll.length();
+		} catch (final BadLocationException e) {
+			throw new RuntimeException(e);
+		}
+		return start;
 	}
 }
