27b8edbf2beb27a819b0f561a55a6ec26def6169
Bernhard Wagner <bernhard.wagner@sbs.ch>
2011-07-07 13:48:38 +0200

    cosmetic change for better readability.

--- test_src/ch/sbs/utils/preptools/DocumentUtilsTest.java
+++ test_src/ch/sbs/utils/preptools/DocumentUtilsTest.java
@@ -10,30 +10,30 @@ import org.junit.Test;
 public class DocumentUtilsTest {
 	@Test
 	public void testDocUtil() throws BadLocationException {
-		final StringBuilder sb = new StringBuilder();
+		final StringBuilder sb1 = new StringBuilder();
 		final StringBuilder sb2 = new StringBuilder();
-		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		sb1.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 		sb2.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-		sb.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
+		sb1.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
 		sb2.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
-		sb.append("<head>");
+		sb1.append("<head>");
 		sb2.append("<head>");
-		sb.append("</head>\n");
+		sb1.append("</head>\n");
 		sb2.append("</head>\n");
-		sb.append("<book>\n");
+		sb1.append("<book>\n");
 		sb2.append("<book>\n");
-		sb.append("<span brl:accents=\"reduced\">Térezia</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">Térezia</span>\n");
 		sb2.append("Térezia\n");
-		sb.append("<span brl:accents=\"reduced\">València</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">València</span>\n");
 		sb2.append("València\n");
-		sb.append("<span brl:accents=\"detailed\">Café</span>\n");
+		sb1.append("<span brl:accents=\"detailed\">Café</span>\n");
 		sb2.append("Café\n");
-		sb.append("</book>\n");
+		sb1.append("</book>\n");
 		sb2.append("</book>\n");
-		sb.append("</dtbook>\n");
+		sb1.append("</dtbook>\n");
 		sb2.append("</dtbook>\n");
-		final Document document = DocumentTestUtil.makeDocument(sb.toString());
-		sb.setLength(0);
+		final Document document = DocumentTestUtil.makeDocument(sb1.toString());
+		sb1.setLength(0);
 		final int count = DocumentUtils.performReplacement(document,
 				"<span.*?>(.*)</span>", "$1");
 		assertEquals(sb2.toString(), document.getText(0, document.getLength()));
@@ -42,30 +42,30 @@ public class DocumentUtilsTest {
 
 	@Test
 	public void testDocUtilReduced() throws BadLocationException {
-		final StringBuilder sb = new StringBuilder();
+		final StringBuilder sb1 = new StringBuilder();
 		final StringBuilder sb2 = new StringBuilder();
-		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		sb1.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 		sb2.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-		sb.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
+		sb1.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
 		sb2.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
-		sb.append("<head>");
+		sb1.append("<head>");
 		sb2.append("<head>");
-		sb.append("</head>\n");
+		sb1.append("</head>\n");
 		sb2.append("</head>\n");
-		sb.append("<book>\n");
+		sb1.append("<book>\n");
 		sb2.append("<book>\n");
-		sb.append("<span brl:accents=\"reduced\">Térezia</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">Térezia</span>\n");
 		sb2.append("Térezia\n");
-		sb.append("<span brl:accents=\"reduced\">València</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">València</span>\n");
 		sb2.append("València\n");
-		sb.append("<span brl:accents=\"detailed\">Café</span>\n");
+		sb1.append("<span brl:accents=\"detailed\">Café</span>\n");
 		sb2.append("<span brl:accents=\"detailed\">Café</span>\n");
-		sb.append("</book>\n");
+		sb1.append("</book>\n");
 		sb2.append("</book>\n");
-		sb.append("</dtbook>\n");
+		sb1.append("</dtbook>\n");
 		sb2.append("</dtbook>\n");
-		final Document document = DocumentTestUtil.makeDocument(sb.toString());
-		sb.setLength(0);
+		final Document document = DocumentTestUtil.makeDocument(sb1.toString());
+		sb1.setLength(0);
 		final int count = DocumentUtils.performReplacement(document,
 				"<span\\s+brl:accents=\"reduced\">(.*)</span>", "$1");
 		assertEquals(sb2.toString(), document.getText(0, document.getLength()));
@@ -75,34 +75,34 @@ public class DocumentUtilsTest {
 
 	@Test
 	public void testDocUtilDetailed() throws BadLocationException {
-		final StringBuilder sb = new StringBuilder();
+		final StringBuilder sb1 = new StringBuilder();
 		final StringBuilder sb2 = new StringBuilder();
-		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		sb1.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 		sb2.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-		sb.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
+		sb1.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
 		sb2.append("<dtbook xmlns:brl=\"http://www.daisy.org/z3986/2009/braille/\">\n");
-		sb.append("<head>");
+		sb1.append("<head>");
 		sb2.append("<head>");
-		sb.append("</head>\n");
+		sb1.append("</head>\n");
 		sb2.append("</head>\n");
-		sb.append("<book>\n");
+		sb1.append("<book>\n");
 		sb2.append("<book>\n");
-		sb.append("<span brl:accents=\"reduced\">Térezia</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">Térezia</span>\n");
 		sb2.append("Térezia\n");
-		sb.append("<span brl:accents=\"reduced\">Térezia</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">Térezia</span>\n");
 		sb2.append("Térezia\n");
-		sb.append("<span brl:accents=\"reduced\">València</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">València</span>\n");
 		sb2.append("València\n");
-		sb.append("<span brl:accents=\"reduced\">València</span>\n");
+		sb1.append("<span brl:accents=\"reduced\">València</span>\n");
 		sb2.append("València\n");
-		sb.append("<span brl:accents=\"detailed\">Café</span>\n");
+		sb1.append("<span brl:accents=\"detailed\">Café</span>\n");
 		sb2.append("Café\n");
-		sb.append("</book>\n");
+		sb1.append("</book>\n");
 		sb2.append("</book>\n");
-		sb.append("</dtbook>\n");
+		sb1.append("</dtbook>\n");
 		sb2.append("</dtbook>\n");
-		final Document document = DocumentTestUtil.makeDocument(sb.toString());
-		sb.setLength(0);
+		final Document document = DocumentTestUtil.makeDocument(sb1.toString());
+		sb1.setLength(0);
 		final int count = DocumentUtils.performReplacement(document,
 				"<span.*?>(.*)</span>", "$1");
 		assertEquals(sb2.toString(), document.getText(0, document.getLength()));
