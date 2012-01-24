bd8cb6c050e8c5aee5a1d20edc896014b6ac2a9a
Bernhard Wagner <bernhard.wagner@sbs.ch>
2012-01-24 14:48:53 +0100

    changed visibility of getMetaInfo to private, removed obsolete comment.

--- src/ch/sbs/plugin/preptools/VFormActionHelper.java
+++ src/ch/sbs/plugin/preptools/VFormActionHelper.java
@@ -6,24 +6,25 @@ import java.util.regex.Pattern;
  * Helper class to factor out common code in VFormActions.
  */
 /**
-	* Copyright (C) 2010 Swiss Library for the Blind, Visually Impaired and Print Disabled
-	*
-	* This file is part of dtbook-preptools.
-	* 	
-	* dtbook-preptools is free software: you can redistribute it
-	* and/or modify it under the terms of the GNU Lesser General Public
-	* License as published by the Free Software Foundation, either
-	* version 3 of the License, or (at your option) any later version.
-	* 	
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	* Lesser General Public License for more details.
-	* 	
-	* You should have received a copy of the GNU Lesser General Public
-	* License along with this program. If not, see
-	* <http://www.gnu.org/licenses/>.
-	*/
+ * Copyright (C) 2010 Swiss Library for the Blind, Visually Impaired and Print
+ * Disabled
+ * 
+ * This file is part of dtbook-preptools.
+ * 
+ * dtbook-preptools is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 public class VFormActionHelper {
 
@@ -44,12 +45,10 @@ public class VFormActionHelper {
 
 	/**
 	 * Utility method to get tool specific metainfo.
-	 * Covariant return type. (VFormPrepTool.MetaInfo is a subclass of
-	 * DocumentMetaInfo.MetaInfo)
 	 * 
 	 * @return tool specific metainfo.
 	 */
-	protected final VFormPrepTool.MetaInfo getMetaInfo() {
+	private final VFormPrepTool.MetaInfo getMetaInfo() {
 		return (VFormPrepTool.MetaInfo) prepToolsPluginExtension
 				.getDocumentMetaInfo().getToolSpecificMetaInfo(
 						VFormPrepTool.PREPTOOL_NAME);
