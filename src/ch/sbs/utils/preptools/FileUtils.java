a3803a177edd1fb81f83bad99df4ba50ecd86554
Bernhard Wagner <bernhard.wagner@sbs.ch>
2012-02-07 14:42:37 +0100

    removed FileUtils.delete, simplified FileUtils.basename

--- src/ch/sbs/utils/preptools/FileUtils.java
+++ src/ch/sbs/utils/preptools/FileUtils.java
@@ -4,52 +4,27 @@ import java.io.File;
 import java.net.URL;
 
 /**
-	* Copyright (C) 2010 Swiss Library for the Blind, Visually Impaired and Print Disabled
-	*
-	* This file is part of dtbook-preptools.
-	* 	
-	* dtbook-preptools is free software: you can redistribute it
-	* and/or modify it under the terms of the GNU Lesser General Public
-	* License as published by the Free Software Foundation, either
-	* version 3 of the License, or (at your option) any later version.
-	* 	
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	* Lesser General Public License for more details.
-	* 	
-	* You should have received a copy of the GNU Lesser General Public
-	* License along with this program. If not, see
-	* <http://www.gnu.org/licenses/>.
-	*/
+ * Copyright (C) 2010 Swiss Library for the Blind, Visually Impaired and Print
+ * Disabled
+ * 
+ * This file is part of dtbook-preptools.
+ * 
+ * dtbook-preptools is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 public class FileUtils {
-
-	public static void delete(final String fileName) {
-
-		File f = new File(fileName);
-
-		if (!f.exists())
-			throw new IllegalArgumentException(
-					"Delete: no such file or directory: " + fileName);
-
-		if (!f.canWrite())
-			throw new IllegalArgumentException("Delete: write protected: "
-					+ fileName);
-
-		// If it is a directory, make sure it is empty
-		if (f.isDirectory()) {
-			final String[] files = f.list();
-			if (files.length > 0)
-				throw new IllegalArgumentException(
-						"Delete: directory not empty: " + fileName);
-		}
-
-		boolean success = f.delete();
-
-		if (!success)
-			throw new IllegalArgumentException("Delete: deletion failed");
-	}
 
 	public static String basename(final URL url) {
 		return url == null ? "" : basename(url.getFile());
@@ -57,8 +32,7 @@ public class FileUtils {
 
 	public static String basename(final String path) {
 
-		return path == null ? "" : path.substring(path
-				.lastIndexOf(File.separatorChar) + 1);
+		return path == null ? "" : new File(path).getName();
 	}
 
 }
--- test_src/ch/sbs/utils/preptools/PropsUtilsTest.java
+++ test_src/ch/sbs/utils/preptools/PropsUtilsTest.java
@@ -2,35 +2,33 @@ package ch.sbs.utils.preptools;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 /**
-	* Copyright (C) 2010 Swiss Library for the Blind, Visually Impaired and Print Disabled
-	*
-	* This file is part of dtbook-preptools.
-	* 	
-	* dtbook-preptools is free software: you can redistribute it
-	* and/or modify it under the terms of the GNU Lesser General Public
-	* License as published by the Free Software Foundation, either
-	* version 3 of the License, or (at your option) any later version.
-	* 	
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	* Lesser General Public License for more details.
-	* 	
-	* You should have received a copy of the GNU Lesser General Public
-	* License along with this program. If not, see
-	* <http://www.gnu.org/licenses/>.
-	*/
+ * Copyright (C) 2010 Swiss Library for the Blind, Visually Impaired and Print
+ * Disabled
+ * 
+ * This file is part of dtbook-preptools.
+ * 
+ * dtbook-preptools is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 public class PropsUtilsTest {
 	private static final String PROPS_FILENAME = "stamp.properties";
@@ -43,20 +41,11 @@ public class PropsUtilsTest {
 	@Before
 	public void setUp() throws IOException {
 		final Properties props = new Properties();
-		final File file = new File(filename);
-		if (file.exists()) {
-			FileUtils.delete(filename);
-		}
 		final FileOutputStream fos = new FileOutputStream(filename);
 		props.put(KEY, VALUE);
 		props.store(fos, "generated file");
 		fos.close();
 
-	}
-
-	@After
-	public void tearDown() {
-		FileUtils.delete(filename);
 	}
 
 	@Test
